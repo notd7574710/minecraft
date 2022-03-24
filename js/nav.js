9b03c1436706d38be5c48dd8cfaf53b3130634d5
notd7574710 <102124169+notd7574710@users.noreply.github.com>
2022-03-24 12:23:45 +0900

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -18,7 +18,7 @@ function button() {//반응형 웹페이지 구현
    nav.appendChild(button);
 }
 
-function pageList() {//탭목록 구현
+function pageList(pageNum) {//탭목록 구현
    var div = document.createElement('div');
    div.setAttribute('class', 'collapse navbar-collapse');
    div.setAttribute('id', 'navbarNav');
@@ -26,11 +26,70 @@ function pageList() {//탭목록 구현
    ul.setAttribute('class', 'nav nav-tabs mr-auto');
 
    var A = document.createElement('li');
+   var B = document.createElement('li');
+   var C = document.createElement('li');
+   var D = document.createElement('li');
+
+   var E = document.createElement('li');
+   var F = document.createElement('li');
+   var G = document.createElement('li');
+   var H = document.createElement('li');
 
    A.setAttribute('class', 'nav-item');
-   A.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav()'>포션</a>";
+   B.setAttribute('class', 'nav-item');
+   C.setAttribute('class', 'nav-item');
+   D.setAttribute('class', 'nav-item');
+   E.setAttribute('class', 'nav-item');
+   F.setAttribute('class', 'nav-item');
+   G.setAttribute('class', 'nav-item');
+   H.setAttribute('class', 'nav-item');
+
+   A.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(1)'>포션 a</a>";
+   B.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(2)'>포션 b</a>";
+   C.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(3)'>포션 c</a>";
+   D.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(4)'>포션 d</a>";
+
+   E.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(5)'>학살자</a>";
+   F.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(6)'>파괴자</a>";
+   G.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(7)'>드론마스터</a>";
+   H.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(8)'>방패병</a>";
+
+   if (pageNum == 1) {
+      A.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(1)'>포션 a</a>";
+   }
+   else if (pageNum == 2) {
+      B.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(2)'>포션 b</a>";
+   }
+   else if (pageNum == 3) {
+      C.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(3)'>포션 c</a>";
+   }
+   else if (pageNum == 4) {
+      D.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(4)'>포션 d</a>";
+   }
+   else if (pageNum == 5) {
+      E.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(5)'>학살자</a>";
+   }
+   else if (pageNum == 6) {
+      F.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(6)'>파괴자</a>";
+   }
+   else if (pageNum == 7) {
+      G.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(7)'>드론마스터</a>";
+   }
+   else if (pageNum == 8) {
+      H.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(8)'>방패병</a>";
+   }
+   else {
+      alert("error! pageNum = " + pageNum);
+   }
 
    ul.appendChild(A);
+   ul.appendChild(B);
+   ul.appendChild(C);
+   ul.appendChild(D);
+   ul.appendChild(E);
+   ul.appendChild(F);
+   ul.appendChild(G);
+   ul.appendChild(H);
    div.appendChild(ul);
 
    var temp = document.createElement('button');
@@ -46,12 +105,13 @@ function pageList() {//탭목록 구현
 }
 //==================================================================================
 
-function createNav() { // nav를 생성하는 함수
+function createNav(pageNum) { // nav를 생성하는 함수
    nav = document.getElementById("nav");
    nav.innerHTML = "";
    nav.setAttribute('class', 'navbar navbar-expand-lg navbar-light fixed-top');
 
-   nav.innerHTML += "<a class='navbar-brand' href='https://github.com/notd7574710/minecraft'>ㅇㅇ</a>";
+   nav.innerHTML += "<a class='navbar-brand' href='https://github.com/notd7574710/minecraft'>minecraft</a>";
    button();//function
-   createTable();
+   pageList(pageNum);//function
+   createTable(pageNum);
 }
