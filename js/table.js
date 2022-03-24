c68f01edf7e73a83f21e2f20efe856fa81c7b90b
notd7574710 <102124169+notd7574710@users.noreply.github.com>
2022-03-24 12:27:14 +0900

    Update table.js

--- js/table.js
+++ js/table.js
@@ -1,6 +1,14 @@
 var classnum;
 function createTable() {
-    
+    var c1 = document.getElementbyId("1");
+    var c2 = document.getElementbyId("2");
+    var c3 = document.getElementbyId("3");
+    var img = document.getElementsbyClassname("img");
+    var ifr = document.getElementsbyClassname("ifr);
+                                              
+    for(int i=0; i<3; i++) {
+        img[i].setAttribute("alt", "테스트" + i);
+        ifr[i].setAttribute("alt", "테스트" + i);
 }
 
 function upper(str) {
